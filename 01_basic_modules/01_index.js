99053675904f1323b6b917dc9f6239b181e6b240
Aritra Bera <aritrabera69@gmail.com>
2024-05-11 12:55:40 +0530

    Add Frist File

---
+++ 01_basic_modules/01_index.js
@@ -0,0 +1,10 @@
+const result = require("./02_math")
+
+
+console.log("Add Value",result.add(5,5));
+
+console.log("Subtraction Value",result.sub(10,5));
+
+console.log("Multiplication Value",result.mul(5,5));
+
+console.log("Division Value",result.div(15,5));
